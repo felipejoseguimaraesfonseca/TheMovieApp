974a66839888c1fca93b3c6aaa0f9e1665f043df
Felipe José Guimarães Fonseca <46244847+felipejoseguimaraesfonseca@users.noreply.github.com>
2020-09-09 19:49:58 -0300

    bar_navigation.xml layout has been added. Series, Favorites and MyAccount activities has been added.

---
+++ app/src/main/java/com/example/themovieapp/view/FavoritesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.themovieapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.themovieapp.R
+
+class FavoritesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+    }
+}
---
+++ app/src/main/java/com/example/themovieapp/view/SeriesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.themovieapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.themovieapp.R
+
+class SeriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_series)
+    }
+}
