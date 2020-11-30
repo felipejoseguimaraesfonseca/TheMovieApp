f7ecd1b9e14ccb3de26d254dc5fe20f2b66e32ad
Felipe José Guimarães Fonseca <46244847+felipejoseguimaraesfonseca@users.noreply.github.com>
2020-11-30 11:57:43 -0300

    UserEntity and UserRepository classes has been updated. UserDao interface has been updated.

--- app/src/main/java/com/example/themovieapp/data/repository/UserDao.kt
+++ app/src/main/java/com/example/themovieapp/data/repository/UserDao.kt
@@ -2,11 +2,12 @@ package com.example.themovieapp.data.repository
 
 import androidx.room.*
 import com.example.themovieapp.data.model.UserEntity
+import io.reactivex.Completable
 
 @Dao
 interface UserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun save(userEntity: UserEntity): Int
+    fun save(userEntity: UserEntity): Completable
 
     @Query("SELECT * FROM user_data WHERE id = :id ")
     fun getUser(id: Int): UserEntity
